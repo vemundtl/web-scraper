ad9b77329b58cdcceddf06f6c52eed0098c11fcb
vemundtl <vemund@thallaug.org>
2021-12-18 00:22:03 +0100

    oppsett og start

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup 
 
-my_url = 'https://store.playstation.com/no-no/category/803cee19-e5a1-4d59-a463-0b6b2701bf7c/1'
+my_url = 'https://store.playstation.com/en-no/category/44d8bb20-653e-431e-8ad0-c0a365f68d2f/1'
 
 # Opening up connection, grabbing page 
 uClient = uReq(my_url)
