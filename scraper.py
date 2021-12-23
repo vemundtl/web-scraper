e196d41693a9d931a5fbad6b2cd36fcea376adbd
vemundtl <vemund@thallaug.org>
2021-12-23 15:53:48 +0100

    litt videre

--- scraper.py
+++ scraper.py
@@ -1,10 +1,10 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup 
 
-my_url = 'https://store.playstation.com/en-no/category/44d8bb20-653e-431e-8ad0-c0a365f68d2f/1'
+url_prisjakt = "https://www.prisjakt.no/tema/dagens-tilbud"
 
 # Opening up connection, grabbing page 
-uClient = uReq(my_url)
+uClient = uReq(url_prisjakt)
 page_html = uClient.read()
 uClient.close()
 
@@ -12,5 +12,7 @@ uClient.close()
 page_soup = soup(page_html, "html.parser")
 
 #grabs column with matches 
-containers = page_soup.findAll("div", {"div", {"class" : "col-12"}}) 
-games = soup.findAll("li", {"class" : "psw-cell"})
+containers = page_soup.findAll("div", {"class" : "offer-product"})
+test = []
+title = containers[0].div.p.text
+test.append(title)
